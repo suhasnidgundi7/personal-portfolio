6acf16ce4c908c398ae7d7a8cbc3503d71af8f26
suhasnidgundi <suhasnidgundi@gmail.com>
2023-06-27 13:29:51 +0530

    Contact Form Implement Working Fine | Insecure!

--- app/contactMe/components/ContactForm.jsx
+++ app/contactMe/components/ContactForm.jsx
@@ -1,19 +1,45 @@
-"use client"
+"use client";
 
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser';
+import React, { useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { useRouteAnimationContext } from "@/context/RouteAnimationContext";
+import { toast } from "react-toastify";
 
 const ContactForm = () => {
+  const [theme, setTheme] = useState("light")
   const form = useRef();
+
+  const { userTheme } = useRouteAnimationContext(); // Accessing userTheme from the context
+
+  useEffect(() => {
+    setTheme(userTheme);
+  }, [userTheme]);
 
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs.sendForm("personal_portfolio", "portfolio_template", form.current, "wIIDAt33JVhuGoGPP")
       .then((result) => {
-        console.log(result.text);
+        toast.success('🎉 Your message has been sent! We will get back to you soon.', {
+          position: "bottom-right",
+          autoClose: 10000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: theme,
+        });
       }, (error) => {
-        console.log(error.text);
+        toast.error('⚠️ Oops! Something went wrong. Please try again later.', {
+          position: "bottom-right",
+          autoClose: 10000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: theme,
+        });
       });
   };
 
@@ -23,36 +49,67 @@ const ContactForm = () => {
       <div className="col-xs-12 col-sm-8">
         <div id="map" className="map">
           <div className="lmpixels-map">
-            <iframe frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?q=San%20Francisco%2C%20S601%20Townsend%20Street%2C%20California%2C%20USA&amp;amp;t=m&amp;amp;z=16&amp;amp;output=embed&amp;amp;iwloc=near&amp;output=embed"></iframe>
+            <iframe
+              frameBorder="0"
+              scrolling="no"
+              marginHeight="0"
+              marginWidth="0"
+              src="https://maps.google.com/maps?q=San%20Francisco%2C%20S601%20Townsend%20Street%2C%20California%2C%20USA&amp;amp;t=m&amp;amp;z=16&amp;amp;output=embed&amp;amp;iwloc=near&amp;output=embed"
+            ></iframe>
           </div>
         </div>
         <div className="block-title">
-          <h3>How Can I <span>Help You?</span></h3>
+          <h3>
+            How Can I <span>Help You?</span>
+          </h3>
         </div>
 
         <form className="contact-form" ref={form} onSubmit={sendEmail}>
-
           <div className="messages"></div>
 
           <div className="controls two-columns">
             <div className="fields clearfix">
               <div className="left-column">
-                <div className="form-group form-group-with-icon" >
-                  <input id="form_name" type="text" name="name" className="form-control" placeholder="" required="required" data-error="Name is required." />
+                <div className="form-group form-group-with-icon">
+                  <input
+                    id="form_name"
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder=""
+                    required="required"
+                    data-error="Name is required."
+                  />
                   <label>Full Name</label>
                   <div className="form-control-border"></div>
                   <div className="help-block with-errors"></div>
                 </div>
 
                 <div className="form-group form-group-with-icon">
-                  <input id="form_email" type="email" name="email" className="form-control" placeholder="" required="required" data-error="Valid email is required." />
+                  <input
+                    id="form_email"
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder=""
+                    required="required"
+                    data-error="Valid email is required."
+                  />
                   <label>Email Address</label>
                   <div className="form-control-border"></div>
                   <div className="help-block with-errors"></div>
                 </div>
 
                 <div className="form-group form-group-with-icon">
-                  <input id="form_subject" type="text" name="subject" className="form-control" placeholder="" required="required" data-error="Subject is required." />
+                  <input
+                    id="form_subject"
+                    type="text"
+                    name="subject"
+                    className="form-control"
+                    placeholder=""
+                    required="required"
+                    data-error="Subject is required."
+                  />
                   <label>Subject</label>
                   <div className="form-control-border"></div>
                   <div className="help-block with-errors"></div>
@@ -60,7 +117,15 @@ const ContactForm = () => {
               </div>
               <div className="right-column">
                 <div className="form-group form-group-with-icon">
-                  <textarea id="form_message" name="message" className="form-control" placeholder="" rows="7" required="required" data-error="Please, leave me a message."></textarea>
+                  <textarea
+                    id="form_message"
+                    name="message"
+                    className="form-control"
+                    placeholder=""
+                    rows="7"
+                    required="required"
+                    data-error="Please, leave me a message."
+                  ></textarea>
                   <label>Message</label>
                   <div className="form-control-border"></div>
                   <div className="help-block with-errors"></div>
@@ -68,15 +133,22 @@ const ContactForm = () => {
               </div>
             </div>
 
-            <div className="g-recaptcha" data-sitekey="6LdqmCAUAAAAAMMNEZvn6g4W5e0or2sZmAVpxVqI"></div>
+            <div
+              className="g-recaptcha"
+              data-sitekey="6LdqmCAUAAAAAMMNEZvn6g4W5e0or2sZmAVpxVqI"
+            ></div>
 
-            <input type="submit" className="button btn-send" value="Send message" />
+            <input
+              type="submit"
+              className="button btn-send"
+              value="Send message"
+            />
           </div>
         </form>
       </div>
       {/* <!-- End of Contact Form --> */}
     </>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import Header from "./components/header"
 import { RouteAnimationContextProvider } from "@/context/RouteAnimationContext"
 import { Analytics } from '@vercel/analytics/react';
 import "bootstrap/dist/css/bootstrap.css"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
   title: '',
@@ -36,6 +38,7 @@ export default function RootLayout({ children }) {
                   <div className="animated-section animated-section-moveFromTop">
                     {children}
                     <Analytics />
+                    <ToastContainer/>
                   </div>
                 </div>
               </div>
