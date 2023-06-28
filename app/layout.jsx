84176da69663ba0edf02d0f603c660e5f6e7f14b
suhasnidgundi <suhasnidgundi@gmail.com>
2023-06-28 12:04:11 +0530

    CSS Optimized!

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="assets/css/reset.css" type="text/css" />
         <link rel="stylesheet" href="assets/css/light.css" type="text/css" />
       </head>
       <body>
