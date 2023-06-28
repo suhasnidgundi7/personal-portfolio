993b30f5d0fb04b8aec8389ef6b7c3d036789c7a
suhasnidgundi <suhasnidgundi@gmail.com>
2023-06-28 12:10:49 +0530

    Revert "Font Optimized!"
    This reverts commit 7e01efed162bb7172a080bd3afc82149633c18ca.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-const withFonts = require('next-fonts');
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = withFonts({
-  enableSvg: true,
-  fonts: {
-    loader: 'default',
-    path: 'https://fonts.googleapis.com/css?family=Poppins:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i',
-  },
-});
+module.exports = nextConfig
