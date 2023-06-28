09d7653bfea9be51885110ce2ec470f8c1fffb8a
suhasnidgundi <suhasnidgundi@gmail.com>
2023-06-28 22:12:48 +0530

    downgrade functionality of contact me!

--- app/contactMe/components/ContactForm.jsx
+++ app/contactMe/components/ContactForm.jsx
@@ -4,214 +4,160 @@ import React, { useEffect, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { useRouteAnimationContext } from "@/context/RouteAnimationContext";
 import { toast } from "react-toastify";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import ContactMap from "./ContactMap";
 import ContactReCAPTCHA from "./ContactReCAPTCHA";
 
 const ContactForm = () => {
-  const form = useRef();
   const [theme, setTheme] = useState("light");
+  const form = useRef();
+
   const { userTheme } = useRouteAnimationContext(); // Accessing userTheme from the context
 
   useEffect(() => {
     setTheme(userTheme);
   }, [userTheme]);
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "personal_portfolio",
+        "portfolio_template",
+        form.current,
+        "wIIDAt33JVhuGoGPP"
+      )
+      .then(
+        (result) => {
+          toast.success(
+            "🎉 Your message has been sent! We will get back to you soon.",
+            {
+              position: "bottom-right",
+              autoClose: 10000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: theme,
+            }
+          );
+        },
+        (error) => {
+          toast.error(
+            "⚠️ Oops! Something went wrong. Please try again later.",
+            {
+              position: "bottom-right",
+              autoClose: 10000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: theme,
+            }
+          );
+        }
+      );
+  };
+
   return (
     <>
       {/* <!-- Contact Form --> */}
       <div className="col-xs-12 col-sm-8">
-        <ContactMap />
+        <div id="map" className="map">
+          <div className="lmpixels-map">
+            <iframe
+              frameBorder="0"
+              scrolling="no"
+              width="100%"
+              height="400px"
+              src="https://maps.google.com/maps?q=San%20Francisco%2C%20S601%20Townsend%20Street%2C%20California%2C%20USA&amp;t=m&amp;z=16&amp;output=embed&amp;iwloc=near&amp;output=embed"
+            ></iframe>
+          </div>
+        </div>
         <div className="block-title">
           <h3>
             How Can I <span>Help You?</span>
           </h3>
         </div>
-        <Formik
-          initialValues={{ name: "", email: "", subject: "", message: "" }}
-          validationSchema={Yup.object({
-            name: Yup.string().required("Name is required"),
-            email: Yup.string()
-              .email("Invalid email")
-              .required("Email is required"),
-            subject: Yup.string().required("Subject is required"),
-            message: Yup.string().required("Message is required"),
-          })}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            console.log(form.current);
-            emailjs
-              .sendForm(
-                process.env.NEXT_PUBLIC_SERVICE_ID,
-                process.env.NEXT_PUBLIC_TEMPLATE_ID,
-                form.current,
-                process.env.NEXT_PUBLIC_PUB_KEY
-              )
-              .then((result) => {
-                alert(result.text);
-                toast.success(
-                  "🎉 Your message has been sent! We will get back to you soon.",
-                  {
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: theme,
-                  }
-                );
-                setSubmitting(false);
-                resetForm();
-              })
-              .catch((error) => {
-                console.log(error);
-                toast.error(
-                  "⚠️ Oops! Something went wrong. Please try again later.",
-                  {
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: theme,
-                  }
-                );
-                setSubmitting(false);
-                resetForm();
-              });
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <>
-              {Object.keys(errors).some(
-                (field) => touched[field] && errors[field]
-              ) && (
-                <div className="alert alert-danger" role="alert">
-                  <div className="container">
-                    <ul>
-                      {touched.name && errors.name && <li>{errors.name}</li>}
-                      {touched.email && errors.email && <li>{errors.email}</li>}
-                      {touched.subject && errors.subject && (
-                        <li>{errors.subject}</li>
-                      )}
-                      {touched.message && errors.message && (
-                        <li>{errors.message}</li>
-                      )}
-                    </ul>
-                  </div>
-                </div>
-              )}
-              <form ref={form} className="contact-form" onSubmit={handleSubmit}>
-                <div className="controls two-columns">
-                  <div className="fields clearfix">
-                    <div className="left-column">
-                      <div className="form-group form-group-with-icon">
-                        <input
-                          type="text"
-                          id="name"
-                          name="name"
-                          className="form-control"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.name}
-                          style={
-                            errors.name && touched.name && errors.name != ""
-                              ? { borderColor: "red" }
-                              : null
-                          }
-                        />
-                        <label htmlFor="name">Full Name</label>
-                        <div className="form-control-border"> </div>
-                      </div>
 
-                      <div className="form-group form-group-with-icon">
-                        <input
-                          type="email"
-                          id="email"
-                          name="email"
-                          className="form-control"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.email}
-                          style={
-                            errors.email && touched.email && errors.email != ""
-                              ? { borderColor: "red" }
-                              : null
-                          }
-                        />
-                        <label htmlFor="email">Email Address</label>
-                        <div className="form-control-border"></div>
-                      </div>
-                      <div className="form-group form-group-with-icon">
-                        <input
-                          type="text"
-                          id="subject"
-                          name="subject"
-                          className="form-control"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.subject}
-                          style={
-                            errors.subject &&
-                            touched.subject &&
-                            errors.subject != ""
-                              ? { borderColor: "red" }
-                              : null
-                          }
-                        />
-                        <label htmlFor="subject">Subject</label>
-                        <div className="form-control-border"></div>
-                      </div>
-                    </div>
-                    <div className="right-column">
-                      <div className="form-group form-group-with-icon">
-                        <textarea
-                          id="message"
-                          name="message"
-                          className="form-control"
-                          rows="7"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.message}
-                          style={
-                            errors.message &&
-                            touched.message &&
-                            errors.message != ""
-                              ? { borderColor: "red" }
-                              : null
-                          }
-                        ></textarea>
-                        <label htmlFor="message">Message</label>
-                        <div className="form-control-border"></div>
-                      </div>
-                    </div>
-                  </div>
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <div className="messages"></div>
 
-                  {/* <ContactReCAPTCHA /> */}
-
+          <div className="controls two-columns">
+            <div className="fields clearfix">
+              <div className="left-column">
+                <div className="form-group form-group-with-icon">
                   <input
-                    disabled={isSubmitting}
-                    type="submit"
-                    className="button btn-send"
-                    value="Send message"
+                    id="name"
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder=""
+                    required="required"
+                    data-error="Name is required."
                   />
+                  <label htmlFor="name">Full Name</label>
+                  <div className="form-control-border"></div>
+                  <div className="help-block with-errors"></div>
                 </div>
-              </form>
-            </>
-          )}
-        </Formik>
+
+                <div className="form-group form-group-with-icon">
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder=""
+                    required="required"
+                    data-error="Valid email is required."
+                  />
+                  <label htmlFor="email">Email Address</label>
+                  <div className="form-control-border"></div>
+                  <div className="help-block with-errors"></div>
+                </div>
+
+                <div className="form-group form-group-with-icon">
+                  <input
+                    id="subject"
+                    type="text"
+                    name="subject"
+                    className="form-control"
+                    placeholder=""
+                    required="required"
+                    data-error="Subject is required."
+                  />
+                  <label htmlFor="subject">Subject</label>
+                  <div className="form-control-border"></div>
+                  <div className="help-block with-errors"></div>
+                </div>
+              </div>
+              <div className="right-column">
+                <div className="form-group form-group-with-icon">
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="form-control"
+                    placeholder=""
+                    rows="7"
+                    required="required"
+                    data-error="Please, leave me a message."
+                  ></textarea>
+                  <label htmlFor="message">Message</label>
+                  <div className="form-control-border"></div>
+                  <div className="help-block with-errors"></div>
+                </div>
+              </div>
+            </div>
+
+            <ContactReCAPTCHA />
+
+            <input
+              type="submit"
+              className="button btn-send"
+              value="Send message"
+            />
+          </div>
+        </form>
       </div>
       {/* <!-- End of Contact Form --> */}
     </>
